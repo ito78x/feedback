bbb01ed197a8175eeebf6bec5002497c8fc09601
ito78x <127704051+ito78x@users.noreply.github.com>
2023-06-20 22:54:42 -0300

    Change forms

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label="Your name", required=True, max_length=100, error_messages={
-        "required": "Your name must not be empty",
-        "max_length": "Please enter a shorter name!"
-    })
-    review_text = forms.CharField(label="Your feedback", widget=forms.Textarea, max_length=200)
-    rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
+from .models import Review
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label="Your name", required=True, max_length=100, error_messages={
+#         "required": "Your name must not be empty",
+#         "max_length": "Please enter a shorter name!"
+#     })
+#     review_text = forms.CharField(label="Your feedback", widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
