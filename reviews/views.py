f8110e14783abd46cfc8385688b51d58ab7cb22b
ito78x <127704051+ito78x@users.noreply.github.com>
2023-06-24 21:53:04 -0300

    Add filter

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,10 @@
 from typing import Any, Dict
+from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .forms import ReviewForm
 from .models import Review
@@ -38,14 +40,15 @@ class ThankYouView(TemplateView):
         context['ito_test'] = "Really works!"
         return context
     
-class ReviewListView(TemplateView):
+class ReviewListView(ListView):
     template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        data = base_query.filter(rating__gt=4)
+        return data
 
 class SingleReviewView(TemplateView):
     template_name = "reviews/single_review.html"
